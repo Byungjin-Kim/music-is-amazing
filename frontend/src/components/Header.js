27dd5ddc845b9c635f6b58dab0b8e21d791fac5a
Byungjin-Kim <composerjins@gmail.com>
2024-06-12 14:13:34 -0500

    added header  css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #333;
+  padding: 1rem;
+`;
+
+const Logo = styled.h1`
+  color: #fff;
+  font-size: 1.5rem;
+  margin: 0;
+`;
+
+const Nav = styled.nav`
+  ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`;
 
 const Header = () => {
     return (
-        <header className='header'>
-         <h1 className='logo'>Music Is Amazing</h1>
-         <nav>
+        <HeaderWrapper>
+     
+         <Logo>Music Is Amazing</Logo>
+         <Nav>
             <ul>
                 <li><a href='#home'>Home</a></li>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#musicInsight'>Music Insight</a></li>        
                 <li><a href='#contact'>Contact</a></li>
             </ul>
-        </nav>
-        </header>
+        </Nav>
+
+        </HeaderWrapper>
     );
 }
 
